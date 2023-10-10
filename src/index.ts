7d2a51c050e9d827b12f67d692670cb8631325b6
Wesley <wesleyscholl@gmail.com>
2023-10-09 22:52:27 -0400

    Updated to new

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,8 @@ interface Res {
 }
 
 export async function markDiscussionCommentAnswer() {
-  const token = getInput("token");
+  const token = getInput("GITHUB_TOKEN");
+  console.log(token);
   token === "INVALID_TOKEN" && setFailed("GitHub token missing or invalid, please enter a GITHUB_TOKEN");
   const eventPayload = require(String(process.env.GITHUB_EVENT_PATH));
   const commentId = eventPayload.comment.node_id;
